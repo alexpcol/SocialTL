ab4aa385d012b1ddb2d72ba8451e69f7442dd949
Alejandro <alejandro.perez@ironbit.com.mx>
2019-07-23 15:03:28 -0500

    Initial Commit

---
+++ SocialTL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialTL
+//
+//  Created by Alejandro Perez on 7/23/19.
+//  Copyright © 2019 Alejandro Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SocialTLTests/SocialTLTests.swift
@@ -0,0 +1,34 @@
+//
+//  SocialTLTests.swift
+//  SocialTLTests
+//
+//  Created by Alejandro Perez on 7/23/19.
+//  Copyright © 2019 Alejandro Perez. All rights reserved.
+//
+
+import XCTest
+@testable import SocialTL
+
+class SocialTLTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
